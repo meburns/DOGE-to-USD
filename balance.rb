964fab1f558d5d7b321615d348825631c995b8c9
Michael Burns <meburns@uzumaki.local>
2014-03-06 10:04:53 -0600

    Added support for negative values

--- balance.rb
+++ balance.rb
@@ -24,6 +24,12 @@ puts Time.new
 puts "USD Avg: #{doge_to_usd_avg_value} = 1 DOGE".yellow
 puts "USD Close: #{doge_to_usd_close_value} = 1 DOGE".yellow
 puts "Total: $#{doge_to_usd_balance.round(2)}".green
-puts "+$#{total_earned}".green
+
+if(total_earned > 0)
+  puts "+$#{total_earned}".green
+else
+  total_earned
+  puts "-$#{total_earned.abs}".red
+end
 
 # puts "93627 Doge was purchased for 95 USD, 1 LTC ($20), and .05 BTC ($20) ~= $145".red
